d47e4963de2f54be04da3624e9d4db517d3e8ece
chhib <davan839@gmail.com>
2022-11-15 10:33:23 +0100

    royal design

--- scraper-royal-design.js
+++ scraper-royal-design.js
@@ -52,7 +52,7 @@ const bigquery = new BigQuery();
                 link: anchor.href,
                 reduction: Math.round(100 * (prices[0] / prices[1] - 1)),
                 price_previous: prices[1],
-                store: 'royal design #2',
+                store: 'royal design #4',
                 title: title,
                 date: new Date().toISOString().split('T')[0],
                 scraped_from: document.URL
@@ -67,19 +67,20 @@ const bigquery = new BigQuery();
 
     console.log(`${productsOnPage.length} products found on ${url}`);
     
+    const nextPageNumber = parseInt(url.match(/page=(\d+).+/)[1], 10) + 1;
     // Insert into BigQuery all items
     if (productsOnPage.length > 0) {
       await insertRowsAsStream(productsOnPage);
     }
 
     // Recursively scrape the next page
-    if (productsOnPage.length < 1) {
+    if (productsOnPage.length < 1 || nextPageNumber < 182) {
       // Terminate if no partners exist
       return productsOnPage
     } else {
 
      // Go fetch the next page ?page=X+1
-      const nextPageNumber = parseInt(url.match(/page=(\d+).+/)[1], 10) + 1;
+      //const nextPageNumber = parseInt(url.match(/page=(\d+).+/)[1], 10) + 1;
        const nextUrl = `https://royaldesign.se/kampanjer/singles-day?CategoryCode=WEBCAT_2_1&CategoryCode=WEBCAT_2_3&CategoryCode=WEBCAT_1_2&CategoryCode=WEBCAT_5_3&CategoryCode=WEBCAT_5_1&CategoryCode=WEBCAT_3_1&CategoryCode=WEBCAT_2_2&CategoryCode=WEBCAT_1_3&CategoryCode=WEBCAT_1_5&CategoryCode=WEBCAT_1_1&CategoryCode=WEBCAT_5_8&CategoryCode=WEBCAT_1_7&CategoryCode=WEBCAT_5_7&CategoryCode=WEBCAT_3_4&CategoryCode=WEBCAT_7_9&CategoryCode=WEBCAT_6_6&Price.max=107865&Price.min=700&page=${nextPageNumber}&sortBy=price_asc`;
 
       return productsOnPage.concat(await extractProducts(nextUrl))
